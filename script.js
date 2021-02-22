2e1869202c9d7d26597b8f59806351c824c5519b
Evgeniya Veselkova <evgeniyaveselk@gmail.com>
2021-02-21 19:32:55 -0500

    Calculate how far text shadow goes

--- script.js
+++ script.js
@@ -6,11 +6,21 @@ function shadow(e) {
   // const height = hero.offsetHeight;
   const { offsetWidth: width, offsetHeight: height } = hero;
   let { offsetX: x, offsetY: y } = e;
+  const walk = 50; //100px
 
   if (this !== e.target) {
     x = x + e.target.offsetLeft;
     y = y + e.target.offsetTop;
   }
+
+  const xWalk = (x / width * walk) - (walk / 2);
+  const yWalk = (y / height * walk) - (walk / 2);
+
+  text.style.textShadow = `${xWalk}px ${yWalk}px 0 rgba(255,0,255,07),
+  ${xWalk * -1}px ${yWalk}px 0 rgba(0,0,255,07),
+  ${yWalk}px ${xWalk* -1}px 0 rgba(0,255,0,07),
+  ${yWalk * -1}px ${xWalk}px 0 rgba(255,255,0,07)
+  `
 }
 
 
